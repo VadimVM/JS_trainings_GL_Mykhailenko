491857f67f183fc9ba43945d2f2111bb9040b71c
VadimVM <vadimvm11@gmail.com>
2021-07-18 18:01:19 +0300

    add app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const value = 5;
+
+console.log(value);
+
+const userName = 'Vadym';
+const age = 33;
+const profession = 'AQA engineer';
+
+const massage = `My name is ${userName}. I'm ${age} years old. I work in GlobalLogic ${profession}.`;
+console.log(massage);
